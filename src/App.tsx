105aad7976dea2e7be77f0cc1f70e8906b4ef286
joshhoneypot <joshuaangugo@gmail.com>
2023-11-02 14:40:49 +0300

    ✅: all imports rendering correctly :code functionality checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Render from './Components/Render'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     App.tsx
+      <Render />
     </div>
   );
 }
---
+++ src/Components/Render.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import About from './Pages/About'
+import Invest from './Pages/Invest'
+import SocialImpact from './Pages/SocialImpact'
+
+function Render() {
+  return (
+    <>
+    <Invest />
+    <About />
+    <SocialImpact />
+    </>
+  )
+}
+
+export default Render
