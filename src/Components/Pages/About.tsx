affdd4c03c4dd890f30a7cfbcc3d5160b71d5c22
joshhoneypot <joshuaangugo@gmail.com>
2023-11-03 19:02:42 +0300

    💄: added the values section in the about page : Added values to the about page

--- src/Components/Pages/About.tsx
+++ src/Components/Pages/About.tsx
@@ -17,6 +17,24 @@ function About() {
           <p>Our investments are therefore not limited to internal ones, but contribute heavily to our community and environment. From training the next scout leaders on sustainable practices, to bridging the employment gap for junior software developers.</p>
         </div>
 
+        <div className="values-section">
+
+          <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1680180166/elewa-group-website/Icons/PNG/Holistic_118_ylipr0.png" alt="holistic solutions" />
+
+          <h2>Holistic solutions</h2>
+          <p>We go beyond a simple patch-up but develop lasting solutions through holistic design.</p>
+
+          <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png" alt='impact'/>
+
+          <h2>Impact</h2>
+          <p>Impact as a direct, or indirect, result. All our respective organizations have underlying theories of change.</p>
+
+          <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690299/elewa-group-website/Icons/PNG/Opendata_fe7h3j.png" alt='Open data'/>
+          
+          <h2>Open data</h2>
+          <p>Sharing is caring. We share what we learn. As proof, we've open-sourced all our internal projects.</p>
+        </div>
+
 
       </div>
 
