46f698f96bc43c001bb09010306739512722948d
joshhoneypot <joshuaangugo@gmail.com>
2023-11-03 20:00:38 +0300

    🎨 : open-closed about component reconstruction : About component code restructure

--- src/Components/Pages/About.tsx
+++ src/Components/Pages/About.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../CommonComponents/Hero'
 import Header from '../CommonComponents/Header'
 import Footer from '../CommonComponents/Footer'
+import aboutUsData, { AboutUsSection } from '../Props/AboutUsPageBodyData'
 import '../../Stylings/About.css'
 
 function About() {
@@ -9,46 +10,21 @@ function About() {
     <div>
       <Header />
       <Hero />
-
       <div className="about-us">
-        <h2>We Care!</h2>
-        <div className="paragraphs">
-          <p>Elewa is a mission-driven organization. We make use of our cooperative and shared culture to drive the needle for the development of people and environment. We care about our growth, but also care deeply about the context surrounding us.</p>
-
-          <p>Our investments are therefore not limited to internal ones, but contribute heavily to our community and environment. From training the next scout leaders on sustainable practices, to bridging the employment gap for junior software developers.</p>
-        </div>
-
-        <div className="values-section">
-          <>
-          <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1680180166/elewa-group-website/Icons/PNG/Holistic_118_ylipr0.png" alt="holistic solutions" />
-
-          <h3>Holistic solutions</h3>
-          <p>We go beyond a simple patch-up but develop lasting solutions through holistic design.</p>
-          </>
-
-          <>
-          <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png" alt='impact'/>
-
-          <h3>Impact</h3>
-          <p>Impact as a direct, or indirect, result. All our respective organizations have underlying theories of change.</p>
-          </>
-          
-          <>
-          <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690299/elewa-group-website/Icons/PNG/Opendata_fe7h3j.png" alt='Open data'/>
-          
-          <h3>Open data</h3>
-          <p>Sharing is caring. We share what we learn. As proof, we've open-sourced all our internal projects.</p>
-          </>
-        </div>
-
-
+        {aboutUsData.map((section: AboutUsSection, index: number) => (
+          <div key={index} className="section">
+            <h2>{section.title}</h2>
+            <div className="paragraphs">
+              {section.paragraphs.map((paragraph: string, idx: number) => (
+                <p key={idx}>{paragraph}</p>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
-
-
-
       <Footer />
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
