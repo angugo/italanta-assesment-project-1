6412d736cac50beb2de86315ad14fe458c451c3f
joshhoneypot <joshuaangugo@gmail.com>
2023-11-03 09:10:31 +0300

    🚧: completed initial set of objects for the menu items in the footer: FooterMenuItems prop object

--- src/Components/Props/FooterMenuItems.ts
+++ src/Components/Props/FooterMenuItems.ts
@@ -3,3 +3,53 @@ interface FooterMenuItems {
     title: string;
     content: string[];
 }
+
+// footer menu items object to be exported
+const footerItems: FooterMenuItems[] = [
+   {
+        title: 'Headquaters',
+        content: [
+            'The Promenade, 19 General',
+            'Mathenge Rd',
+            'Nairobi, Kenya',
+        ],
+    },
+
+    {
+        title: 'Content',
+        content: [
+            "T +254 78 92 27 755",
+            "E info@elewa.ke"
+        ],
+    },
+
+    {
+        title: 'Navigation',
+        content: [
+            'About us',
+            'Social impact',
+            'Invest',
+            'News',
+            'Contact'
+        ],
+    },
+
+    {
+        title: 'Brands',
+        content: [
+            'Elewa',
+            'Italanta',
+           ' Venture labs'
+        ],
+    },
+
+    {
+        title: 'Privacy',
+        content: [
+            'Terms and conditions',
+            'Cookie preferences'
+        ],
+    },
+
+    
+]
