a1e32f39581ad4bd2eb39d8850b05d274f105609
joshhoneypot <joshuaangugo@gmail.com>
2023-11-03 18:53:51 +0300

    💄: added the We care section along with its paragraphs : We care section on the about page

--- src/Components/Pages/About.tsx
+++ src/Components/Pages/About.tsx
@@ -8,6 +8,20 @@ function About() {
     <div>
       <Header />
       <Hero />
+
+      <div className="about-us">
+        <h1>We Care!</h1>
+        <div className="paragraphs">
+          <p>Elewa is a mission-driven organization. We make use of our cooperative and shared culture to drive the needle for the development of people and environment. We care about our growth, but also care deeply about the context surrounding us.</p>
+
+          <p>Our investments are therefore not limited to internal ones, but contribute heavily to our community and environment. From training the next scout leaders on sustainable practices, to bridging the employment gap for junior software developers.</p>
+        </div>
+
+
+      </div>
+
+
+
       <Footer />
     </div>
   )
