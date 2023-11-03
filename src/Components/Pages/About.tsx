3acd28a816b6885a178f87120dff56533188c532
joshhoneypot <joshuaangugo@gmail.com>
2023-11-03 19:50:01 +0300

    🎨 : about page styling update : About.css styling the values section

--- src/Components/Pages/About.tsx
+++ src/Components/Pages/About.tsx
@@ -19,21 +19,26 @@ function About() {
         </div>
 
         <div className="values-section">
-
+          <>
           <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1680180166/elewa-group-website/Icons/PNG/Holistic_118_ylipr0.png" alt="holistic solutions" />
 
-          <h2>Holistic solutions</h2>
+          <h3>Holistic solutions</h3>
           <p>We go beyond a simple patch-up but develop lasting solutions through holistic design.</p>
+          </>
 
+          <>
           <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690301/elewa-group-website/Icons/PNG/coorperative_kzlzrg.png" alt='impact'/>
 
-          <h2>Impact</h2>
+          <h3>Impact</h3>
           <p>Impact as a direct, or indirect, result. All our respective organizations have underlying theories of change.</p>
-
+          </>
+          
+          <>
           <img src="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690299/elewa-group-website/Icons/PNG/Opendata_fe7h3j.png" alt='Open data'/>
           
-          <h2>Open data</h2>
+          <h3>Open data</h3>
           <p>Sharing is caring. We share what we learn. As proof, we've open-sourced all our internal projects.</p>
+          </>
         </div>
 
 
